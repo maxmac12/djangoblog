ba28e095da92a56aa4e575fe8b5eb97a447095d1
Max MacCamy <maxmac12@gmail.com>
2016-03-08 20:48:58 -0800

    Updated production.py

--- mysite/production.py
+++ mysite/production.py
@@ -1,5 +1,6 @@
-from settings import *
+from mysite.settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = 
+ALLOWED_HOSTS = ['ec2-54-213-86-237.us-west-2.compute.amazonaws.com', 'localhost']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
